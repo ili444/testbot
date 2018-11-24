85d96e853262b32a98e252221607e7af1a538b7f
ili444 <39112695+ili444@users.noreply.github.com>
2018-11-24 22:27:38 +0700

    Update 321.py

--- 321.py
+++ 321.py
@@ -17,7 +17,7 @@ from docx import Document
 import zipfile
 from bs4 import BeautifulSoup
 from flask import Flask, request
-TOKEN = os.environ['token']
+TOKEN = os.environ[token]
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
