c3af56545fc4bc20c16fe59f7c7f875dcb830eee
ili444 <39112695+ili444@users.noreply.github.com>
2018-11-27 19:01:21 +0700

    Update 321.py

--- 321.py
+++ 321.py
@@ -120,7 +120,7 @@ def msg_hand(message):
         num = 1
         user.num = num
         if message.content_type == 'document':
-            if user.type_print is not None:
+            if user.type_print == None:
                 file_id = message.document.file_id
                 user.file_id = file_id
                 file_info = bot.get_file(file_id)
@@ -130,8 +130,7 @@ def msg_hand(message):
                 user.file_name = file_name
                 bot.send_message(message.from_user.id, 'Выберите услугу:', reply_markup=inline_markup())
             else:
-                bot.edit_message_text(chat_id=chat_id, message_id=callback.message.message_id,
-                                  text='Хорошо, выберите кол-во копий:', reply_markup=markup)
+                bot.edit_message_text(chat_id, text='Хорошо, выберите кол-во копий:', reply_markup=markup)
         if 'https' in message.text:
             url = message.text
             result = urllib.request.urlopen(url)
