c27e16ef94f323ed056cbd9e774ebf68d2f27be1
ili444 <39112695+ili444@users.noreply.github.com>
2018-12-07 22:51:08 +0700

    Update dbworker.py

--- dbworker.py
+++ dbworker.py
@@ -4,7 +4,7 @@ import shelve
 
 # Пытаемся узнать из базы «состояние» пользователя
 def get_current_state(user_id):
-    with shelve.open('db_file') as db:
+    with shelve.open('db_file.py') as db:
         try:
             return db[user_id]
         except KeyError:  # Если такого ключа почему-то не оказалось
@@ -12,7 +12,7 @@ def get_current_state(user_id):
 
 # Сохраняем текущее «состояние» пользователя в нашу базу
 def set_state(user_id, value):
-    with shelve.open('db_file') as db:
+    with shelve.open('db_file.py') as db:
         try:
             db[user_id] = value
             return True
