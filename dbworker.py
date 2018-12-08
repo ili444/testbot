d7d0b7390575905142c715a91fd21ba27443c12c
ili444 <39112695+ili444@users.noreply.github.com>
2018-12-08 17:27:51 +0700

    Update dbworker.py

--- dbworker.py
+++ dbworker.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-import config
 import shelve
 
 # Пытаемся узнать из базы «состояние» пользователя
 def get_current_state(user_id):
     with shelve.open('db_file.py') as db:
         try:
-            return db[user_id].decode()
+            return db[user_id]
         except KeyError:  # Если такого ключа почему-то не оказалось
-            return config.States.S_START.value  # значение по умолчанию - начало диалога
+            return '0'  # значение по умолчанию - начало диалога
 
 # Сохраняем текущее «состояние» пользователя в нашу базу
 def set_state(user_id, value):
