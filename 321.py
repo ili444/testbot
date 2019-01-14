13ca2c1edaa49dca4be1eab8e06ee8ae1b60261d
ili444 <39112695+ili444@users.noreply.github.com>
2019-01-14 21:39:55 +0700

    Update 321.py

--- 321.py
+++ 321.py
@@ -933,7 +933,6 @@ def got_payment(message):
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    print(request.data)
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "HTTP 200 OK", 200
 
