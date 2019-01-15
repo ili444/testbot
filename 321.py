0ff9ee21d0252cdc99163a08fe7607974aa5295d
ili444 <39112695+ili444@users.noreply.github.com>
2019-01-15 13:56:46 +0700

    Update 321.py

--- 321.py
+++ 321.py
@@ -971,7 +971,9 @@ def getMessage():
 @server.route('/' + 'PAYMENTS', methods=['POST'])
 def Check_Payments():
     chat_id = request.form['label']
+    print(chat_id)
     total_price = request.form['amount']
+    print(total_price)
     user = user_dict[chat_id]
     if total_price == user.total_price:
         mark_up.finish_payment(chat_id)
